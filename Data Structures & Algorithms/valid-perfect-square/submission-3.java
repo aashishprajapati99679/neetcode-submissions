408c42d648b5d2f254f36f0ac8949a5400ac4210
Ashish Prajapati <155898594+aashishprajapati99679@users.noreply.github.com>
2026-09-05 22:16:57 +0530

    Add: valid-perfect-square - submission-3

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 0;
+        int end = num;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            long  sqr = (long) mid * mid;
+            if(sqr == num){
+                return true;
+            }else if(sqr > num){
+
+                end = mid - 1;
+            }else{
+                start = mid +1;
+            }
+        }
+        return false;
+    }
+}
